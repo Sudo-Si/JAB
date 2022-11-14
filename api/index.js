4cc178732302817fd398226581053403e927963b
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-15 11:48:29 +1300

    api done ready to merge

--- api/index.js
+++ api/index.js
@@ -2,20 +2,36 @@ const express = require("express");
 const app = express();
 const dotenv =require("dotenv");
 const mongoose = require('mongoose');
-const authRoute = require('./routes/auth')
-const userRoute = require('./routes/users')
-const postRoute = require('./routes/posts')
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer =require('multer')
  dotenv.config();
  app.use(express.json());
  mongoose.connect(process.env.MONGO_URL)
  .then(console.log("Connected to Mongo"))
  .catch((err)=>console.log(err));
+ const storage = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, "images")
+    }, filename:(req, file, cb)=>{
+        cb(null,"hello2.jpg")
+    },
+ });
 
+ const upload = multer({storage:storage});
+ app.post("/api/upload", upload.single("file"), (req, res)=>{
+    res.status(200).json(" File has been uploaded ");
+ })
  app.use("/api/auth", authRoute);
  app.use("/api/users", userRoute);
  app.use("/api/posts", postRoute);
+ app.use("/api/categories", categoryRoute);
+
+
  const PORT =process.env.PORT;
-// console.log("yo")
+
 app.listen(PORT, ()=>{
     console.log("Backend is running on port "+ PORT)
 })
