62184437df8b35ff2d6f5fe34a44800040a5c984
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-14 17:42:24 +1300

    client side created

---
+++ client/src/components/StyledNavbar.js
@@ -0,0 +1,62 @@
+import { NavLink , Link } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import simon from '../images/simon.png'
+
+import About from '../pages/About'
+// import Single from "../pages/single/Single";
+import Write from "./Write";
+import Register from "../pages/Register";
+const  Navbar =()=>
+{
+    const user =
+    true;
+    // false;
+    return (
+        <nav className="navbar">
+        <NavLink 
+        to ='/'
+        className={({isActive}) =>(isActive ? 'link active': 'link')}
+        >
+        </NavLink> 
+        
+        <div className="navbar">
+            <div className="top-left">
+                <i class="top-icon fab fa-instagram-square"></i>
+                <i class="top-icon fab fa-tiktok"></i>
+                <i class="top-icon fab fa-linkedin"></i>
+                <i class="top-icon fab fa-etsy"></i>
+            </div>
+            <div className="links top-center">
+            <NavLink to='/' className="lnk" element={<Home/>}>Home</NavLink>
+            
+            <NavLink to='/about' className="lnk" element={<About/>}>About</NavLink>
+            <NavLink to='/write' className="lnk" element={<Write />}>Write</NavLink>
+
+            {/* <NavLink to='/contact' className="lnk" element={<About/>}>Contact</NavLink> */}
+            {/* <NavLink to='/write' className="lnk" element={<About/>}>Write</NavLink> */}
+            {/* <NavLink to='/login' className="lnk" element={<Login/>}>Login</NavLink>  */}
+            {/* <NavLink to='/logout' className="lnk" element={user ? <Login/> :}>Logout</NavLink> */}
+            </div>
+            <div className="top-right">
+                {user ? (   
+                <img 
+                className="top-search-icon" 
+                src={simon} 
+                alt=""/>
+                ) :(
+                    <>
+                <Link to='/login' className="lnk" element={<Login/>}>Login</Link> 
+                <Link to='/register' className="lnk" element={<Register/>}>Register</Link> 
+                </>
+                )}
+             
+            <i class="fas fa-search"></i>
+              
+            </div>
+        </div>
+        </nav>
+        
+        )
+}
+export default Navbar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+// import {  Outlet } from "react-router-dom";
+// import Products from "./Products";
+// import About from './About'
+// import Navbar from "./Navbar";
+
+import Header from "../components/Header";
+import Posts from "../components/Posts";
+import Sidebar from "../components/Sidebar";
+
+const  Home =()=>
+{
+    return (
+        <> 
+   <Header />
+   <div className="home"> 
+   <Posts/>
+   <Sidebar />
+   </div>
+        </>
+    )
+}
+export default Home;
