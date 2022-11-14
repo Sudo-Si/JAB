5409764e891bdf624ae373496040829511153d48
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-15 00:58:46 +1300

    user and auth routes done,  next is posts and category

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const dotenv =require("dotenv");
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
  dotenv.config();
- mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to Mongo")).catch((err)=>console.log(err));
+ app.use(express.json());
+ mongoose.connect(process.env.MONGO_URL)
+ .then(console.log("Connected to Mongo"))
+ .catch((err)=>console.log(err));
+
+ app.use("/api/auth", authRoute);
+ app.use("/api/users", userRoute);
  const PORT =process.env.PORT;
 // console.log("yo")
 app.listen(PORT, ()=>{
