2aad88d337ec1916a606542ef524b36a22a4171a
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-15 11:22:01 +1300

    Post routes complete crud operations working

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv =require("dotenv");
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
  dotenv.config();
  app.use(express.json());
  mongoose.connect(process.env.MONGO_URL)
@@ -12,6 +13,7 @@ const userRoute = require('./routes/users')
 
  app.use("/api/auth", authRoute);
  app.use("/api/users", userRoute);
+ app.use("/api/posts", postRoute);
  const PORT =process.env.PORT;
 // console.log("yo")
 app.listen(PORT, ()=>{
