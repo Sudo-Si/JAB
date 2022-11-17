b687a9d8e85d5214f6ed530c364bee5464678c59
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-11-17 14:39:31 +1300

    working on the write post logic not working

--- client/src/components/Write.js
+++ client/src/components/Write.js
@@ -1,18 +1,59 @@
-import React from 'react'
-import Outdoors from '../images/Outdoors.png'
+import React, { useContext, useState } from 'react';
+// import { useContext, useRef } from 'react';
+import axios from 'axios'
+import {Context} from "../context/Context";
 export default function write() {
+
+  const [title, setTitle]= useState('');
+  const [desc, setDesc]= useState('');
+  const [file, setFile]= useState(null);
+  const {user}= useContext(Context);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const newPost ={
+      username: user.username,
+      title, 
+      desc, 
+      file
+      };
+      if(file){
+        const data = new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        newPost.photo =filename;
+      
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+        window.location.replace("/post/" + res.data._id)
+    } catch (err) {
+      console.log(err.response.data)
+      
+    }
+    console.log("user"+ user)
+  };
   return (
     <div>
       <div className='write'>
-    <img src={Outdoors} alt="" className='writeImg'/>
-        <form action="" className='writeForm'>
+        {file &&    <img src={URL.createObjectURL(file)} alt="" className='writeImg'/>
+        }
+ 
+        <form action="" className='writeForm' onSubmit={handleSubmit}>
             <div className='writeFormGroup'>
               
                 <label htmlFor="fileInput">  
                     <i className="writeIcon fa-regular fa-plus"></i> 
                 </label>
-                <input type="file" id="fileInput" style={{display:"none"}} /> 
-                <input type="text" placeholder='Title' id="fileInput" className='writeInput' autoFocus={true} />
+                <input type="file" id="fileInput" style={{display:"none"}} onChange={e=>setFile(e.target.files[0])} /> 
+                <input type="text" placeholder='Title' id="fileInput" className='writeInput' autoFocus={true}  
+                onChange={e=>setTitle(e.target.value)} />
                 </div>
                 <div className='writeFormGroup'> 
                     <textarea 
@@ -20,10 +61,11 @@ export default function write() {
                         placeholder='Whats the yarn?' 
                         id="fileInput" 
                         className='writeInput writeText'  
+                        onChange={e=>setDesc(e.target.value)}
                     ></textarea>
            
            
-            </div> <button className='writeSubmit' > Publish</button>
+            </div> <button className='writeSubmit'type='submit' > Publish</button>
         </form>
       </div>
       </div>
